442230c58cc2302adf7bbd5d9ddd53c1e8b4c56f
Harvey Ball <me@harveyball.com>
2016-02-01 15:48:03 +0000

    chore(UserStatus): reformat previous status into inline list
    The previous status list is now inline and only displays the previous
    status description with a title tag that shows the description and
    since date.

--- web/static/js/components/StatusHistoryItem.js
+++ web/static/js/components/StatusHistoryItem.js
@@ -4,8 +4,9 @@ import * as Formatter from '../utils/Format'
 export default class StatusHistoryItem extends Component {
   render() {
     var since = Formatter.formatDate(this.props.since, Formatter.defaultDateFormat)
+    var title = this.props.status + " since " + since
     return (
-      <li>{this.props.status} since {since}</li>
+      <li title={title}>{this.props.status}</li>
     )
   }
 }
