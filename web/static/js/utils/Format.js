08b88b2ace4c4b36c5fbb4e1b3754c945a29dab7
Harvey Ball <me@harveyball.com>
2016-03-03 11:20:12 +0000

    fix(date_format): create date object from atomic items
    Previously, the `Format.js` util woould attempt to format a date by
    taking in a string of a date and converting it to a Date object based on
    the string. This would work in Chrome and Internet Explorer because
    their Date implementations were not strict to the standard and were more
    forgiving. However, this caused a fatal error in Firefox as it is closer
    to the standard and not any date string will work.

--- web/static/js/utils/Format.js
+++ web/static/js/utils/Format.js
@@ -3,6 +3,15 @@ import dateFormatter from 'dateformat-light'
 export const defaultDateFormat = 'dddd, dS mmmm, yyyy "at" HH:MM'
 
 export function formatDate(dateString, dateFormat) {
-  var makeDate = new Date(dateString)
+  var dateTimeElements = dateString.split(" ")
+  if (dateTimeElements.length !== 2) return dateString
+
+  var dateElements = dateTimeElements[0].split("-")
+  var timeElements = dateTimeElements[1].split(":")
+
+  if (dateElements.length !== 3 || timeElements.length !== 3) return dateString
+
+  var makeDate = new Date(dateElements[0], (dateElements[1] - 1), dateElements[2],
+                          timeElements[0], timeElements[1], timeElements[2], 0)
   return dateFormatter(makeDate, dateFormat)
 }
