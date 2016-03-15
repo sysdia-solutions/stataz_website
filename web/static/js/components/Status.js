6ef6c5ccb4bdf0e07298ffd53172c456873cc9ab
Harvey Ball <me@harveyball.com>
2016-03-15 10:56:47 +0000

    feat(Status): format since in link title tag to readable date-time
    The since used in the title tag on a status link has been updated so
    it's format is more readable rather than the machine stored DateTime
    string.

--- web/static/js/components/Status.js
+++ web/static/js/components/Status.js
@@ -1,9 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
+import * as Formatter from '../utils/Format'
+
 export default class Status extends Component {
+  formatSince(since) {
+    return Formatter.formatDate(since, Formatter.defaultDateFormat)
+  }
+
   render() {
-    var since = (this.props.since ? this.props.status + " since " + this.props.since : "")
+    var since = (this.props.since ? this.props.status + " since " + this.formatSince(this.props.since) : "")
     var href = "/search/" + this.props.status
     return (
       <Link className="status profile-link" title={since} to={href}>
